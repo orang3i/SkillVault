025d423e76d0c3d6f1f60a29606b0714ec59f8c4
orang3i <orangeinfotainment@gmail.com>
2026-01-23 19:04:08 +0530

    Hello World (test)

--- src/main/java/com/orang3i/skillvault/SkillVaultApplication.java
+++ src/main/java/com/orang3i/skillvault/SkillVaultApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SkillVaultApplication {
 
+    //Hello World
     public static void main(String[] args) {
         SpringApplication.run(SkillVaultApplication.class, args);
     }
